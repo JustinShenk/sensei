2ca6c0a66717569099488edf4e044d4bbd8e6996
Justin Shenk <shenkjustin@gmail.com>
2017-04-14 17:02:43 +0200

    Improve data extraction functions

--- analyze.py
+++ analyze.py
@@ -7,7 +7,7 @@ from glob import glob
 from datetime import datetime
 
 
-def load_data(data_path):
+def dat_to_dict(data_path):
     '''Return dictionary `data` from string `data_path`
     '''
     os.path.join(data_path, '1.dat')
@@ -21,8 +21,8 @@ def get_baseline(data):
     baselines = []
     for k, v in data.items():
         if 'baseline' in v:
-            print(k, v)
             baselines.append((k, v))
+    print(baselines)
     # Get most recent baseline
     return sorted(baselines)[-1][1].split(' ')[-1]
 
@@ -47,13 +47,29 @@ def get_distances(data):
     return time_object, dists
 
 
-def load_data_files(data_folder_path='data'):
+def get_data_files(data_folder_path='data'):
     data_folder = os.path.join(os.getcwd(), data_folder_path)
     files = []
     for file in glob(data_folder + "/*/*"):
         if '.dat' in file:
             files.append(file)
     return files
+
+
+def load_all_data(data_folder_path='data'):
+    '''Return dictionary with all data in `data_folder_path` folder.
+    '''
+    files = get_data_files('data')
+    data = [dat_to_dict(x) for x in files]
+    return data
+
+
+def get_subject(data, subject_number='20', trial_number='1'):
+    '''Return subject's data.
+    '''
+    for d in data:
+        if subject_number in d:
+            return d[subject_number][trial_number]
 
 
 def plot(time_objects, dists):
